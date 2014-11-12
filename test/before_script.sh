cc105e294a2fa726f1eaf117119927d161f440e8
Anton <anton@elfet.ru>
2014-11-12 12:09:50 +0300

    Update before_script.sh

--- test/before_script.sh
+++ test/before_script.sh
@@ -1,11 +1,13 @@
-if ! type hhvm > /dev/null; then
+if [ "$TRAVIS_PHP_VERSION" == "hhvm" ]; then
+  echo "HHVM"
+else 
   sudo apt-get update -qq 
   sudo apt-get install -y -qq libssh2-1-dev libssh2-php
   pecl install -f ssh2-beta < test/.noninteractive
   php -m | grep ssh2
-fi  
+fi;
 
-composer self-update
-composer install --dev
+travis_retry composer self-update
+travis_retry composer install --no-interaction --prefer-source --dev
 
 echo `whoami`":1234" | sudo chpasswd
